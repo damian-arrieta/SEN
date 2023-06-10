dba98bc9cc58c9984f8671f0c8766df5ed25fd04
arrdamian <arrdamian@icloud.com>
2023-06-10 12:23:03 -0300

    build navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Navbar() {
 
@@ -9,6 +9,8 @@ export default function Navbar() {
     {name: "Categoria", link: '/'},
   ];
 
+  let [open, setOpen]=useState(false);
+
   return (
     <div className='shadow-md w-full fixed top-0 left-0'>
       <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
@@ -17,7 +19,10 @@ export default function Navbar() {
             SEN
           </span>
         </div>
-        <ul className='md:flex cd:items-center'>
+        <div onClick={()=>setOpen(!open)} className='text-3x1 absolute right-8 top-6 cursor-pointer md:hidden'>
+          <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
+        </div>
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
           {
             links.map((link)=>(
               <li key={link.name} className='md:ml-8 text-xl'>
