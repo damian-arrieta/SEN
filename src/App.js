d6ca260f6cc3063eb3e51428b447327271e1e8c2
arrdamian <arrdamian@icloud.com>
2023-06-26 17:34:52 -0300

    Init Services component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Services from './components/Services';
 
 export default function App() {
   return (
     <> 
       <Navbar />
       <Hero />
+      <Services />
     </>
   )
 }
